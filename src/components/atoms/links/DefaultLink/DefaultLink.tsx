79c321f56a3c1843a778150dc91df5d73c1c6cd7
aguillenAC <aguillen@acity.com.pe>
2023-11-03 17:53:47 -0500

    disabled state for link component

--- src/components/atoms/links/DefaultLink/DefaultLink.tsx
+++ src/components/atoms/links/DefaultLink/DefaultLink.tsx
@@ -1,12 +1,14 @@
 import { Theme } from "@/interfaces";
 import clsx from "clsx";
-import { FC } from "react";
+import { FC, useState, useCallback } from "react";
 
 import styles from "./defaultLink.module.scss";
 
 interface CustomProps {
   label: string;
   theme: Theme;
+  visited?: boolean;
+  disabled?: boolean;
 }
 export type DefaultLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> &
   CustomProps;
@@ -15,15 +17,31 @@ export const DefaultLink: FC<DefaultLinkProps> = ({
   className,
   label,
   theme,
+  onClick,
+  visited: defaultVisited = false,
+  disabled = false,
   ...rest
 }) => {
+  const [visited, setVisited] = useState(defaultVisited);
+
+  const handleClick = useCallback(
+    (e: React.MouseEvent<HTMLAnchorElement>) => {
+      if (disabled) return;
+      setVisited(true);
+      onClick && onClick(e);
+    },
+    [setVisited]
+  );
+
   return (
     <a
       {...rest}
+      onClick={handleClick}
       className={clsx(
         className,
         styles.defaultLink,
-        styles[`defaultLink-${theme}`]
+        styles[`defaultLink-${theme}`],
+        visited && styles[`defaultLink-visited`]
       )}
     >
       {label}
